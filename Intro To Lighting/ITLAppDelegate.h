ebce1969a584db5a6f8fe6f0b4c6bab191b30cb3
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-07-14 13:38:37 -0400

    Initial Commit

---
+++ Intro To Lighting/ITLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ITLAppDelegate.h
+//  Intro To Lighting
+//
+//  Created by Hamdan Javeed on 2013-07-14.
+//  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Intro To Lighting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Intro To Lighting
+//
+//  Created by Hamdan Javeed on 2013-07-14.
+//  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ITLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ITLAppDelegate class]));
+    }
+}
